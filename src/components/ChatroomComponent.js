9f13af43df40b01622643c0d639770a466a3526c
NicL9923 <49288482+NicL9923@users.noreply.github.com>
2020-05-24 11:07:33 -0500

    Fixed ordering of messages in DB call and styled chatroom a little more.

--- src/components/ChatroomComponent.js
+++ src/components/ChatroomComponent.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import './stylesheets/ChatroomComponent.css';
+
+/* TODOs:
+-Put edit button by signed-in name to pop up a simple text/button form
+to change user's nickname
+-Format Date object function
+-Similar to BlogComponent, pull only X most recent, with button to retrieve
+Y more older messages if clicked
+
+*/
 
 class ChatroomComponent extends React.Component {
     constructor(props) {
@@ -20,7 +30,7 @@ class ChatroomComponent extends React.Component {
         this.authCheck();
 
         //Listen (real-time) for changes (ie new messages sent)
-        this.chatroom = firebase.firestore().collection("chatroom").onSnapshot(snapshot => {
+        this.chatroom = firebase.firestore().collection("chatroom").orderBy("timestamp").onSnapshot(snapshot => {
             snapshot.forEach((snap) => {
                 this.setState({ messages: [...this.state.messages, snap.data()] });
             });
@@ -102,6 +112,11 @@ class ChatroomComponent extends React.Component {
 
         this.setState({ msg: "", messages: [] });
     }
+
+    formatDateObject = () => {
+        //TODO: Will take in Date object from map function and format
+        //it to something pleasant
+    }
     
     render() {
         return(
@@ -115,52 +130,57 @@ class ChatroomComponent extends React.Component {
                     <meta name="keywords" content="motorcycle, motorcycles, moto, motobible, rider, riding, cruising, cruiser, bike, bible, motorcyclist, 
                     motorcyclist's, minigames, chatroom, blog, resources, license"/>
                 </Helmet>
-                <div className="card card-body my-1">
-                    {this.state.userEmail ? (
-                    !this.state.hasJoined ? (
-                        <div className="TBD">
-                            <p>{this.state.userEmail}</p>
-                            <input name="chatName" placeholder="Chat Name" value={this.state.chatName} onChange={this.handleChange} /><br />
-                            <button onClick={this.handleJoin}>Join</button>
-                        </div>
-                        ) : (
-                        <div className="TBD">
+
+                <div className="card my-1">
+                    <h2 className="card-header">Live Chatroom</h2>
+
+                    <div className="card-body">
+                        {this.state.userEmail ? (
+                        !this.state.hasJoined ? (
                             <div className="TBD">
-                                {this.state.messages.map(message => {
-                                    if (message.email === this.state.userEmail) {
-                                        return (
-                                            <div className="message" key={message.timestamp}>
-                                                <p id="currentUser">{message.chatName}:</p>
-                                                <p>{message.content}</p>
-                                                <p>{new Date(message.timestamp).toString()}</p>
-                                            </div>
-                                        );
-                                    }
-
-                                    else {
-                                        return (
-                                            <div className="TBD" key={message.timestamp}>
-                                                <p id="otherUsers">{message.chatName}:</p>
-                                                <p>{message.content}</p>
-                                                <p>{new Date(message.timestamp).toString()}</p>
-                                            </div>
-                                        );
-                                    }
-                                })}
+                                <p>Email: {this.state.userEmail}</p>
+                                <input name="chatName" className="form-control" placeholder="Chat Name" value={this.state.chatName} onChange={this.handleChange} /><br />
+                                <button onClick={this.handleJoin}>Join</button>
                             </div>
+                            ) : (
+                            <div className="TBD">
+                                <div className="boxOfMessages">
+                                    {this.state.messages.map(message => {
+                                        if (message.email === this.state.userEmail) {
+                                            return (
+                                                <div className="message" key={message.timestamp}>
+                                                    <p id="currentUser">{message.chatName}:</p>
+                                                    <p>{message.content}</p>
+                                                    <p><small>{new Date(message.timestamp).toString()}</small></p>
+                                                </div>
+                                            );
+                                        }
 
-                            <input name="msg" placeholder="Enter your message here" onChange={this.handleChange} value={this.state.message} />
-                            <button onClick={this.sendMessage}>Send</button>
+                                        else {
+                                            return (
+                                                <div className="message" key={message.timestamp}>
+                                                    <p id="otherUsers">{message.chatName}:</p>
+                                                    <p>{message.content}</p>
+                                                    <p><small>{new Date(message.timestamp).toString()}</small></p>
+                                                </div>
+                                            );
+                                        }
+                                    })}
+                                </div>
+                                <div className="input-group my-1 mt-2">
+                                    <input name="msg" className="form-control mr-2"placeholder="Enter your message here" onChange={this.handleChange} value={this.state.message} />
+                                    <button className="btn btn-primary" onClick={this.sendMessage}>Send</button>
+                                </div>
 
-                            <p>Signed in as: {this.state.userEmail} ({this.state.chatName})</p>
-                        </div>
-                    )
-                    ) : (
-                        <div className="TBD">
-                            <p>You must have an account and be logged in to chat.</p>
-                        </div>
-                    )
-                }
+                                <p className="mt-3"><small><b>Signed in as:</b> {this.state.userEmail} ({this.state.chatName})</small></p>
+                            </div>
+                        )
+                        ) : (
+                            <div className="TBD">
+                                <p>You must have an account and be logged in to chat.</p>
+                            </div>
+                        )}
+                    </div>
                 </div>
 
                 <div className="card card-body my-1">
